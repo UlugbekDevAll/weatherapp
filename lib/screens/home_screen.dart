b836bd10dd2c72a4f72716cd34c66336d42d8813
abduxalilsupe <118978574+abduxalilsupe@users.noreply.github.com>
2024-01-02 09:48:45 +0500

    update weather app

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,10 +1,95 @@
+import 'dart:ui';
+
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 
 class HomeScreen extends StatelessWidget {
   const HomeScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold();
+    return Scaffold(
+      backgroundColor: Colors.black,
+      extendBodyBehindAppBar: true,
+      appBar: AppBar(
+        backgroundColor: Colors.transparent,
+        elevation: 0,
+        // systemOverlayStyle:
+        //     const SystemUiOverlayStyle(statusBarBrightness: Brightness.dark),
+      ),
+      body: Padding(
+          padding: const EdgeInsets.fromLTRB(40, 1.2 * kToolbarHeight, 40, 20),
+          child: SizedBox(
+            height: MediaQuery.of(context).size.height,
+            child: Stack(
+              children: [
+                Align(
+                  alignment: const AlignmentDirectional(9, -0.3),
+                  child: Container(
+                    height: 300,
+                    width: 300,
+                    decoration: const BoxDecoration(
+                      shape: BoxShape.circle,
+                      color: Colors.deepPurple,
+                    ),
+                  ),
+                ),
+                Align(
+                  alignment: const AlignmentDirectional(-9, -0.3),
+                  child: Container(
+                    height: 300,
+                    width: 300,
+                    decoration: const BoxDecoration(
+                      shape: BoxShape.circle,
+                      color: Colors.deepPurple,
+                    ),
+                  ),
+                ),
+                Align(
+                  alignment: const AlignmentDirectional(0, -1.2),
+                  child: Container(
+                    height: 300,
+                    width: 600,
+                    decoration: const BoxDecoration(
+                      shape: BoxShape.rectangle,
+                      color: Color(0xffffab40),
+                    ),
+                  ),
+                ),
+                BackdropFilter(
+                  filter: ImageFilter.blur(sigmaX: 100.00, sigmaY: 100.00),
+                  child: Container(
+                    decoration: const BoxDecoration(color: Colors.transparent),
+                  ),
+                ),
+                SizedBox(
+                  width: MediaQuery.of(context).size.width,
+                  height: MediaQuery.of(context).size.height,
+                  child: const Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Text(
+                        "Tashkent",
+                        style: TextStyle(
+                          color: Colors.white,
+                          fontWeight: FontWeight.w300,
+                        ),
+                      ),
+                      SizedBox(height: 8),
+                      Text(
+                        "Good Morning",
+                        style: TextStyle(
+                          color: Colors.white,
+                          fontSize: 25,
+                          fontWeight: FontWeight.bold,
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+              ],
+            ),
+          )),
+    );
   }
 }
